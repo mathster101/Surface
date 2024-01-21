9ebfe71936d9ef89ae01c0efd0c34a7ef67615ea
Mathster101 <mathewsimonpanicker@gmail.com>
2024-01-21 18:12:38 -0500

    interface between agents and dmaster appears to  work

--- Surface.py
+++ Surface.py
@@ -23,19 +23,20 @@ class Surface:
         print("Datamaster is online")
         proc_queues = []
         while True:
-            time.sleep(0.1)#remove later!
+            time.sleep(0.001)#remove later!
             #check if main has sent new queues
             while self.main2dmaster.empty() == False:
                 new_queue = self.main2dmaster.get(block=True,timeout = 1)
                 proc_queues.append(new_queue)
+            #print(proc_queues)
+            #print("____________________________")
             #service one request from each process
             for incoming,outgoing in proc_queues:
-                print(incoming)
                 if incoming.empty():
                     continue
                 orderrcvd = incoming.get()
-                command, netqueueId = orderrcvd
-                print(command)
+                print(orderrcvd)
+                command, netqueueId = orderrcvd                
                 if netqueueId not in self.netqdata:
                     self.netqdata[netqueueId] = []
 
@@ -48,6 +49,7 @@ class Surface:
                 elif command[0] == "PUT":
                     data = command[1]
                     self.netqdata[netqueueId].append(data)
+                print(self.netqdata)
 
 
 
--- test.py
+++ test.py
@@ -1,16 +1,22 @@
 import Surface
 import multiprocessing as mp
-
+import time
 
 
 def main():
     surf = Surface.Surface()
     man = mp.Manager()
     s,r = man.Queue(), man.Queue()
+    a,b = man.Queue(), man.Queue()
     surf.main2dmaster.put([s,r])
-    while True:
-        pass
-
+    surf.main2dmaster.put([a,b])
+    s.put([["PUT",69],1234])
+    s.put([["PUT",70],1234])
+    s.put([["PUT",71],1234])
+    s.put([["PUT",72],1234])
+    s.put([["PUT",72],1235])
+    a.put([["PUT",100],1235])
+    time.sleep(4)#make this bigger if needed
 
 
 main()
